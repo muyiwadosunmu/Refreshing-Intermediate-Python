3215db5c4625462d2c112c98b7d4df57786388c3
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-08-23 12:26:52 +0100

    First commit

---
+++ lists.py
@@ -0,0 +1,12 @@
+#  Lists -> ordered, mutable, allow duplicate elements
+
+myList = ["banana", "cherry", "apple"]
+
+myList2 = list()
+print(myList, myList2)
+
+myList3 = [5, True, "apple", "apple"]
+print(myList3)
+
+item = myList[0]
+print(item)
